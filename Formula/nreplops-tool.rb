007b557624614b94984db3ac5a3881430e40d851
Matti Hänninen <matti@mjhanninen.com>
2024-01-17 08:42:15 +0200

    nreplops-tool 0.2.0

--- Formula/nreplops-tool.rb
+++ Formula/nreplops-tool.rb
@@ -1,16 +1,15 @@
 class NreplopsTool < Formula
   desc "Non-interactive nREPL client for shell scripts and command-line"
   homepage "https://github.com/mjhanninen/nreplops-tool"
-  url "https://github.com/mjhanninen/nreplops-tool/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "0c4a456c7f24bd099f423ae3f7071093cbb5bf471f12913bffba78e89daa922a"
+  url "https://github.com/mjhanninen/nreplops-tool/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "58c4993d6a5e9be297bc6e659dbd3666935d33efab774126596942a470e736a0"
   license "Apache-2.0"
 
   depends_on "pandoc" => :build
   depends_on "rust" => :build
-  depends_on "borkdude/brew/babashka" => :test
 
   def install
-    system "cargo", "install", *std_cargo_args
+    system "cargo", "install", *std_cargo_args(path: "nr")
     system "pandoc", "-s", "-f", "markdown", "-t", "man", "-o", "man/nr.1", "man/nr.1.md"
     man1.install "man/nr.1"
   end
