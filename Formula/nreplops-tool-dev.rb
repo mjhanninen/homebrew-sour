6ce2540c6b54f5ed97a7ef50a07fb40998a02cd9
Matti Hänninen <matti@mjhanninen.com>
2024-02-01 08:36:35 +0200

    nreplops-tool-dev 0.3.1-rc.2

--- Formula/nreplops-tool-dev.rb
+++ Formula/nreplops-tool-dev.rb
@@ -5,8 +5,8 @@ class NreplopsToolDev < Formula
   conflicts_with "mjhanninen/sour/nreplops-tool",
     because: "nreplops-tools is the main version of this package"
   url "https://github.com/mjhanninen/nreplops-tool.git",
-    tag: "v0.3.1-rc.1",
-    revision: "f813495c6c06542a9b23d22e7f5e0c07573bc44e"
+    tag: "v0.3.1-rc.2",
+    revision: "5547d86721953e5c290a766459c6c41d0a23fa48"
 
   depends_on "pandoc" => :build
   depends_on "rust" => :build
